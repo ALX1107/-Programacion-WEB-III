d90f2d882123d7bb9870bb60b6a329b0fa87fb07
ALX1107 <134250862+ALX1107@users.noreply.github.com>
2025-09-23 21:55:08 -0400

    Add files via upload

---
+++ Practica1/ejercicio1.js
@@ -0,0 +1,18 @@
+//Ejercicio1
+//Crear una función que cuente cuántas veces aparece cada vocal en un texto y devuelva el
+//resultado en un objeto.
+//let obj = miFuncion(“euforia”)
+//console.log(obj) // { a: 1, e: 1, i: 1, o: 1, u: 1 }
+function f(t) {
+  let r = { a: 0, e: 0, i: 0, o: 0, u: 0 };
+  for (let x of t.toLowerCase()) {
+    if (r.hasOwnProperty(x)) {
+      r[x]++;
+    }
+  }
+  return r;
+}
+
+// Ejemplo de uso
+let obj = f("Matematica");
+console.log(obj);
